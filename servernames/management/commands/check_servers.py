3a1bf23535e44e58d736e6cfe7a93ac1b18a5fa9
Azerm1 <azerazete250@gmail.com>
2026-08-10 11:16:44 +0100

    fix emails

--- servernames/management/commands/check_servers.py
+++ servernames/management/commands/check_servers.py
@@ -2,28 +2,36 @@ from django.core.management.base import BaseCommand
 from servernames.models import servername
 from servernames.services import remaining
 from servernames.email import send_notification
+
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        serverenames = servername.objects.all()
-        for server in serverenames:
+        servers = servername.objects.all()
+
+        for server in servers:
             days = remaining(server.enddate)
+
             if days == 30 and not server.notification_30:
-                send_notification(server,30)
+                send_notification(server, 30)
                 server.notification_30 = True
                 server.save()
+
             elif days == 15 and not server.notification_15:
-                send_notification(server,15)
+                send_notification(server, 15)
                 server.notification_15 = True
                 server.save()
-            elif 1<days<=7 and not server.notification_7:
-                send_notification(server,7)
+
+            elif 1 < days <= 7 and not server.notification_7:
+                send_notification(server, 7)
                 server.notification_7 = True
                 server.save()
+
             elif days == 1 and not server.notification_1:
-                send_notification(server,1)
+                send_notification(server, 1)
                 server.notification_1 = True
                 server.save()
+
             elif days < 0 and not server.notification_expired:
-                send_notification(server,0)
+                send_notification(server, 0)
                 server.notification_expired = True
                 server.save()
